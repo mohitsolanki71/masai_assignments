89a7cb2590114021849ae55f03a49c2d8493895d
mohit <mohit_fw12_226@masai.school>
2021-10-20 03:11:19 +0530

    19oct assignment

---
+++ 19oct assign/component/navbar.js
@@ -0,0 +1,21 @@
+function navbar(){
+
+    return `<h3><a href="search.html">Home</a></h3>
+
+        <div id="options">
+            
+            <h3><a href="latest.html">Lastest Recipe</a></h3>
+            <h3><a href="recipeOfDay.html">Recipe of Day</a></h3>
+        </div>`
+}
+
+function input(){
+
+    return `
+                <input type="text" id="search" placeholder="search recipe">
+                <button id="show">Search</button>
+            `
+
+}
+
+export {navbar, input};
---
+++ 19oct assign/scripts/search.js
@@ -0,0 +1,40 @@
+async function getData(url){
+
+    try{
+        let res = await fetch(url);
+
+        let data  = await res.json();
+
+        return data;
+    } catch(e){
+
+        console.log(e);
+    }
+}
+
+function append(recipes, container){
+
+    recipes.forEach(({strMeal, strInstructions , strMealThumb})=>{
+
+        let div = document.createElement("div");
+
+        let image = document.createElement("img");
+        image.src = strMealThumb;
+
+        let p2 = document.createElement("h2");
+        p2.innerText =strMeal;
+
+        let p1 = document.createElement("p");
+        p1.innerText =strInstructions;
+
+        div.append(image ,p2 ,p1);
+
+        container.append(div);
+
+    });
+}
+
+
+
+export { getData, append };
+
