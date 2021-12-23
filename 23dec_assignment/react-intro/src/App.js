8d7e1a3315bcb79d2f528fa3d9ce77e92cf07d4a
mohitsolanki71 <91531587+mohitsolanki71@users.noreply.github.com>
2021-12-23 23:54:44 +0530

    23dec_assignment

---
+++ 23dec_assignment/react-intro/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="first">
+        <h1>Mobile Opertating System</h1>
+        <li >android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </div>
+
+      <div className="first">
+        <h1>Mobile Menufecturers</h1>
+        <li >Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </div>
+    </div>
+  );
+}
+
+export default App;
